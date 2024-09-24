da84109d0f3a02f207beeeaa73d3f963008ee119
Ofinn <dmitrijzajc175@gmail.com>
2024-09-24 18:45:38 +0300

    requirements

---
+++ src/run_server.py
@@ -0,0 +1,34 @@
+import logging
+import uvicorn
+import asyncio
+from asgiref.wsgi import WsgiToAsgi
+
+from src import app, bot
+from settings import settings
+
+
+async def main() -> None:
+    webserver = uvicorn.Server(
+        config=uvicorn.Config(
+            app=WsgiToAsgi(app),
+            port=settings.PORT,
+            use_colors=False,
+            host="0.0.0.0",
+        )
+    )
+
+    await bot.bot.set_webhook(
+        url=settings.WEBHOOK_URL,
+        allowed_updates=bot.dp.resolve_used_update_types(),
+        drop_pending_updates=True
+    )
+    logging.info('Бот запущен')
+    await webserver.serve()
+    await bot.bot.session.close()
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logging.error('Бот остановлен')
---
+++ src/settings.py
@@ -0,0 +1,27 @@
+from os import getenv as get
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI: str = (
+        'postgresql://'
+        f'{get("POSTGRES_USER")}:'
+        f'{get("POSTGRES_PASSWORD")}@'
+        f'{get("DB_HOST")}:{get("DB_PORT")}/{get("POSTGRES_DB")}'
+    )
+    SECRET_KEY: str = get('SECRET_KEY')
+
+
+class Settings:
+    PORT: int = int(get('PORT', 5000))
+    TELEGRAM_TOKEN: str = get('TELEGRAM_TOKEN')
+    WEB_URL: str = get('WEB_URL', 'http://localhost:5000')
+    WEBHOOK_PATH: str = f'/bot/{TELEGRAM_TOKEN}'
+    WEBHOOK_URL: str = f'{WEB_URL}{WEBHOOK_PATH}'
+
+
+settings = Settings()
