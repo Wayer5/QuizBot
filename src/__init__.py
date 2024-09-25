4f1527e68b1fbc0041ba3f94e0343b398704da1c
Никита Ряднов <nriadnov@gmial.com>
2024-09-25 20:48:09 +0200

    Исправление импорта и добавление запятой

--- src/__init__.py
+++ src/__init__.py
@@ -6,11 +6,11 @@ from settings import Config
 
 from MedStat_Solutions_team3.src.models import (
     Category,
-    QuizResult,
     Question,
     Quiz,
+    QuizResult,
+    UserAnswer,
     Variant,
-    UserAnswer
 )
 
 app = Flask(__name__)
