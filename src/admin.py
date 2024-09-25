b32f3843306fc30f4e729d3888911287518a276b
Ofinn <dmitrijzajc175@gmail.com>
2024-09-25 13:16:49 +0300

    Up flask version to 3.0.3

--- src/admin.py
+++ src/admin.py
@@ -1,19 +1,6 @@
-from flask import request, session
 from flask_admin import Admin
-from flask_babelex import Babel
 
 from . import app
 
-# Babel - библиотека для перевода интерфейса на другие языки
-babel = Babel(app)
 # Создания экземпляра админ панели
 admin = Admin(app, name='MedStat_Solutions', template_mode='bootstrap4')
-
-
-@babel.localeselector
-def get_locale() -> None:
-    """Перевод админ панели на другие языки."""
-    if request.args.get('lang'):
-        session['lang'] = request.args.get('lang')
-    # Меняется только параметр 'ru'
-    return session.get('lang', 'ru')
