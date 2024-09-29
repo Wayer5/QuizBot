8a668f8afaf06ab35c756db5f63ea23849943ba1
Никита Ряднов <nriadnov@gmial.com>
2024-09-30 01:18:52 +0200

    add_telegram-user-model_and_migration

--- src/models.py
+++ src/models.py
@@ -278,3 +278,60 @@ class UserAnswer(db.Model):
         default=False,
         comment='Флаг, указывающий, является ли ответ правильным.',
     )
+
+
+class TelegramUser(db.Model):
+
+    """Модель для хранения информации о пользователях Telegram."""
+
+    __tablename__ = 'telegram_users'
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        comment='Уникальный идентификатор записи в таблице.',
+    )
+    telegram_id = db.Column(
+        db.BigInteger,
+        unique=True,
+        nullable=False,
+        comment='Уникальный идентификатор пользователя в Telegram.',
+    )
+    first_name = db.Column(
+        db.String(100),
+        nullable=False,
+        comment='Имя пользователя в Telegram.',
+    )
+    last_name = db.Column(
+        db.String(100),
+        nullable=True,
+        comment='Фамилия пользователя в Telegram.',
+    )
+    username = db.Column(
+        db.String(100),
+        nullable=True,
+        unique=True,
+        comment='Имя пользователя в Telegram (username).',
+    )
+    language_code = db.Column(
+        db.String(10),
+        nullable=True,
+        comment='Язык пользователя в формате IETF.',
+    )
+    is_premium = db.Column(
+        db.Boolean,
+        default=False,
+        comment='Указывает, есть ли у пользователя премиум в Telegram.',
+    )
+    added_to_attachment_menu = db.Column(
+        db.Boolean,
+        default=False,
+        comment='Указывает, добавил ли пользователь бота в меню вложений.',
+    )
+    created_on = db.Column(
+        db.DateTime,
+        default=datetime.utcnow,
+        comment='Дата и время создания записи.',
+    )
+
+    def __repr__(self) -> str:
+        return f'<TelegramUser id={self.telegram_id}'
