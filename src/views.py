c513ff0ac06ec7ad4cd38ddc0ad8b9aa322d8d55
babichdenis <55616713+babichdenis@users.noreply.github.com>
2024-10-02 06:08:43 +0300

    Update views.py

--- src/views.py
+++ src/views.py
@@ -95,7 +95,7 @@ def delete_profile(user_id: int) -> Response:
 
     user_crud.remove(user)
     return render_template('categories.html')
-=======
+
 @app.route('/', methods=['GET'])
 async def categories() -> str:
     """Вывод страницы категорий."""
