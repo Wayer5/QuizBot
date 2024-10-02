012572f68eef3ea9036fa18a36c177b3c0202f88
Pavel <ya.skillet-02@ya.ru>
2024-10-02 14:10:28 +0300

    revert changes from the last commit

--- src/views.py
+++ src/views.py
@@ -66,7 +66,7 @@ async def auntification() -> str:
 
 
 @app.route('/', methods=['GET'])
-async def index() -> str:
+async def categories() -> str:
     """Вывод страницы категорий."""
     categories = category_crud.get_multi()
     return render_template('categories.html', categories=categories)
