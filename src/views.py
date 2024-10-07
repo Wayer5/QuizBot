3831887371e97a8a719565bfd062ab1d786b9073
babichdenis <babichdenis@me.com>
2024-10-07 20:20:26 +0300

    full_results

--- src/views.py
+++ src/views.py
@@ -82,8 +82,33 @@ def profile() -> Response:
 
     total_questions = sum(result.total_questions for result in quiz_results)
     correct_answers_count = sum(
-        result.correct_answers_count for result in quiz_results
-    )
+        result.correct_answers_count for result in quiz_results)
+
+    # Добавляем вопросы к каждому результату
+    for result in quiz_results:
+        result.questions = []
+        user_answers = user_answer_crud.get_results_by_user(user.id)
+
+        # Получаем все вопросы по викторине
+        questions = question_crud.get_all_by_quiz_id(result.quiz_id)
+
+        for question in questions:
+            user_answer = next(
+                (ua for ua in user_answers if ua.question_id == question.id), None)
+            correct_answer_text = next(
+                (v.title for v in question.variants if v.is_right_choice), None)
+
+            # Получаем текст ответа пользователя
+            user_answer_text = next((v.title for v in question.variants if v.id == (
+                user_answer.answer_id if user_answer else None)), 'Не отвечено')
+
+            result.questions.append({
+                'title': question.title,  # Текст вопроса
+                'user_answer': user_answer_text,  # Текст ответа пользователя
+                'correct_answer': correct_answer_text,  # Правильный ответ
+                # Пояснение
+                'explanation': question.explanation if hasattr(question, 'explanation') else None
+            })
 
     return render_template(
         'user_profile.html',
@@ -100,18 +125,21 @@ def delete_profile() -> Response:
     """Удаляет профиль пользователя, сохраняя результаты викторин."""
     user = current_user
     quiz_results = quiz_result_crud.get_results_by_user(user.id)
+
     # Обновляем результаты викторин, чтобы убрать связь с пользователем
     for result in quiz_results:
         result.user_id = None
         user_crud.update(result, {'user_id': None})
 
     user_answers = user_answer_crud.get_results_by_user(user.id)
+
     # Обновляем ответы пользователя, убирая связь с таблицей пользователи
     for answer in user_answers:
         answer.user_id = None
         user_crud.update(answer, {'user_id': None})
 
     user_crud.remove(current_user)
+
     return render_template('categories.html')
 
 
